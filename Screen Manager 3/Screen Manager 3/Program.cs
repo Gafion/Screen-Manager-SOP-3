dbfb729a1b6371071b0226768b2f3a50d0dcd252
Gafion <kroll94@gmail.com>
2024-04-03 13:00:08 +0200

    update

---
+++ Screen Manager 3/Screen Manager 3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Screen_Manager_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
